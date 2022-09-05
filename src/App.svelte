d88bb01d65c52d7d7dd9cd997858283db2835575
m-housni <mohamed.housni@um5s.net.ma>
2022-09-05 16:15:58 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+import AsideLeft from "./lib/AsideLeft.svelte";
+import Main from "./lib/Main.svelte";
+import AsideRight from "./lib/AsideRight.svelte";
+
+
+  
+
+</script>
+
+
+<div class="flex min-h-screen  2xl:max-w-screen-2xl 2xl:mx-auto 2xl:border-x-2 2xl:border-gray-200 dark:2xl:border-zinc-700 ">
+ 
+  <AsideLeft />
+
+  <Main />
+
+  <AsideRight />
+</div>
+
