6367e9d443250ce5fcfdb088c9ff0726af48463a
m-housni <mohamed.housni@um5s.net.ma>
2022-09-07 07:50:40 +0100

    commit

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -25,12 +25,12 @@
   <!-- <img class="object-cover rounded-lg h-full w-full" src="/inception.jpg" alt=""> -->
   <div class="flex float-right p-6">
     <svg on:click={prev}
-      class="m-1 h-7 w-7 rounded-full border p-1 hover:border-red-600 hover:fill-red-600 dark:fill-white dark:hover:fill-red-600"
+      class="cursor-pointer m-1 h-7 w-7 rounded-full border p-1 hover:border-red-600 hover:fill-red-600 dark:fill-white dark:hover:fill-red-600"
       xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
       <path d="M13.293 6.293L7.58 12l5.7 5.7 1.41-1.42 -4.3-4.3 4.29-4.293Z"></path>
     </svg>
     <svg on:click={next}
-      class="m-1 h-7 w-7 rounded-full border p-1 hover:border-red-600 hover:fill-red-600 dark:fill-white dark:hover:fill-red-600"
+      class="cursor-pointer m-1 h-7 w-7 rounded-full border p-1 hover:border-red-600 hover:fill-red-600 dark:fill-white dark:hover:fill-red-600"
       xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
       <path d="M10.7 17.707l5.7-5.71 -5.71-5.707L9.27 7.7l4.29 4.293 -4.3 4.29Z"></path>
     </svg>
